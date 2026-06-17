de1afa8b0d47217c60c5031124ee3b24708394d3
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-17 23:38:23 +0530

    Implemented beans of both classes into main file

--- spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
+++ spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
@@ -9,9 +9,13 @@ public class Spring1Application {
 	@Autowired
 	Function1 f1 = null;
 
+	@Autowired
+	Function2 f2 = null;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Spring1Application.class, args)
 		f1.op1();
+		f2.op2();
 	}
 
 }
