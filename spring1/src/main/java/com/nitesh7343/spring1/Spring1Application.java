cb45740a963873067831ca9a6d445931afac2ba1
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-17 23:43:55 +0530

    Formatted

--- spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
+++ spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Spring1Application.class, args)
+
 	}
 
 }
