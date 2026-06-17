3f6ade9e1ebe7544a3da9bb22f13aef6765c4df2
Nitesh7343 <niteshsingh7343@gmail.com>
2026-06-17 23:44:59 +0530

    initial state comparison

--- spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
+++ spring1/src/main/java/com/nitesh7343/spring1/Spring1Application.java
@@ -7,9 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring1Application {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Spring1Application.class, args)
-
+		System.out.println("Hello world");
 	}
 
 }
