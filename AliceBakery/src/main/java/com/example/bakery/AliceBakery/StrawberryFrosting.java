14071b62484d68d61c957e194115b92b066d3d29
Nitesh7343 <niteshsingh7343@gmail.com>
2026-07-14 15:15:52 +0530

    Frosting type : Strawberry

---
+++ AliceBakery/src/main/java/com/example/bakery/AliceBakery/StrawberryFrosting.java
@@ -0,0 +1,12 @@
+package com.example.bakery.AliceBakery;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StrawberryFrosting implements Frosting{
+
+    @Override
+    public String getFrostingType() {
+        return "Strawberry Frosting";
+    }
+}
