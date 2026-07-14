74f8ea0f94fa22a59d3897738f5ac224550f3c10
Nitesh7343 <niteshsingh7343@gmail.com>
2026-07-14 15:15:35 +0530

    Frosting type : Chocolate

---
+++ AliceBakery/src/main/java/com/example/bakery/AliceBakery/ChocolateFrosting.java
@@ -0,0 +1,12 @@
+package com.example.bakery.AliceBakery;
+
+import org.springframework.stereotype.Component;
+
+@Component()
+public class ChocolateFrosting implements Frosting {
+
+    @Override
+    public String getFrostingType() {
+        return "Chocolate Frosting";
+    }
+}
